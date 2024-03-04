39deef33a8606db775f92da287ff9efd04bb8385
wblackmon <wayne.blackmon@outlook.com>
2024-03-03 19:04:47 -0600

    Unit test module complete.

---
+++ CleanArchitecture.UnitTests/Features/LeaveTypes/Queries/GetLeaveTypeListQueryHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Features.LeaveTypes.Queries.GetLeaveTypes;
+using CleanArchitecture.Application.MappingProfiles;
+using CleanArchitecture.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.UnitTests.Features.LeaveTypes.Queries
+{
+    public class GetLeaveTypeListQueryHandlerTests
+    {
+        private readonly Mock<ILeaveTypeRepository> _mockRepo;
+        private Mock<IAppLogger<GetLeaveTypesQueryHandler>> _mockIAppLogger;
+        private IMapper _mapper;
+        public GetLeaveTypeListQueryHandlerTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+            _mockIAppLogger = new Mock<IAppLogger<GetLeaveTypesQueryHandler>>();
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<LeaveTypeProfile>();
+            });
+            
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypesListTest()
+        {
+            var handler = new GetLeaveTypesQueryHandler(_mockRepo.Object, _mapper, _mockIAppLogger.Object);
+
+            var result = await handler.Handle(new GetLeaveTypesQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<LeaveTypeDto>>();
+            result.Count.ShouldBe(3);
+        }
+
+
+    }
+}
--- CleanArchitecture.UnitTests/Mocks/MockLeaveRequestRepository.cs
+++ CleanArchitecture.UnitTests/Mocks/MockLeaveRequestRepository.cs
@@ -14,29 +14,24 @@ namespace CleanArchitecture.UnitTests.Mocks
     {
         public static Mock<ILeaveRequestRepository> GetMockLeaveTypeRepository()
         {
-
-        //    public class LeaveRequest : BaseEntity
-        //{
-        //    public DateTime StartDate { get; set; }
-        //    public DateTime EndDate { get; set; }
-        //    public LeaveType? LeaveType { get; set; }
-        //    public int LeaveTypeId { get; set; }
-        //    public DateTime DateRequested { get; set; }
-        //    public string RequestComments { get; set; } = string.Empty;
-        //    public bool Approved { get; set; }
-        //    public bool Cancelled { get; set; }
-        //    public string RequestingEmployeeId { get; set; } = string.Empty;
-        //}
-        var leaveRequests = new List<LeaveRequest>
+            var leaveRequests = new List<LeaveRequest>
             {
-new LeaveRequest { Id = 1, LeaveTypeId = 1, StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(10), DateRequested = DateTime.Now, RequestingEmployeeId = "1", Approved = false, Cancelled = false },
+                new LeaveRequest { Id = 1, LeaveTypeId = 1, StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(10), DateRequested = DateTime.Now, RequestingEmployeeId = "1", Approved = false, Cancelled = false },
                 new LeaveRequest { Id = 2, LeaveTypeId = 2, StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(10), DateRequested = DateTime.Now, RequestingEmployeeId = "2", Approved = false, Cancelled = false },
                 new LeaveRequest { Id = 3, LeaveTypeId = 3, StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(10), DateRequested = DateTime.Now, RequestingEmployeeId = "3", Approved = false, Cancelled = false }
             };
             var mockRepo = new Mock<ILeaveRequestRepository>();
 
+            mockRepo.Setup(repo => repo.GetAllLeaveRequests()).ReturnsAsync(leaveRequests);
 
-        
+            mockRepo.Setup(repo => repo.GetLeaveRequestByid(It.IsAny<int>()))
+                .ReturnsAsync((int id) => leaveRequests.FirstOrDefault(lr => lr.Id == id));
+
+            mockRepo.Setup(repo => repo.GetLeaveRequestsByEmployeeId(It.IsAny<string>()))
+                .ReturnsAsync((string employeeId) => leaveRequests.Where(lr => lr.RequestingEmployeeId == employeeId).ToList());
+  
+
             return mockRepo;
+        }
     }
 }
